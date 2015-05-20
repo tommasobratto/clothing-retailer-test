86064accdbd0b4a2cb8777d34b42b64843bef230
tommasobratto <tbratto22@gmail.com>
2015-05-20 18:06:56 +0100

    added add and remove from cart feature

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -1,9 +1,35 @@
 var app = angular.module('ClothMaker', []);
 
-app.controller('catalogueController', function($scope, $http) {
+app.controller('shopController', function($scope, $http) {
+  $scope.cart = [];
+
   var APIRequest = $http.get('http://localhost:3000/api');
 
   APIRequest.then(function(response) {
     $scope.catalogue = response.data;
   });
+
+  $scope.addToCart = function(product) {
+    if(product.quantity > 0) {
+      if($.inArray(product, $scope.cart) === -1)   {
+        $scope.cart.push(product);
+      } 
+      
+      $scope.checkStock(product);
+      // need to add a jQuery function to show a 'item added to cart',
+      // to write a test for it happening in the browser and a unit test.
+    } else {
+      console.log('Item out of stock');
+    }
+  }
+
+  $scope.checkStock = function(product) {
+    var productIndex = $scope.catalogue.indexOf(product);
+    $scope.catalogue[productIndex].quantity -= 1;
+  }
+
+  $scope.removeFromCart = function(product) {
+    var productIndex = $scope.cart.indexOf(product);
+    $scope.cart.splice(productIndex, 1);
+  } 
 });
--- test/features/ClothMakerAppFeaturesSpec.js
+++ test/features/ClothMakerAppFeaturesSpec.js
@@ -5,13 +5,31 @@ describe('ClothMaker app', function() {
   });
 
   it('should be able to display the name of a product', function() {
-
     var productName = element(by.className('product-name')).getText();
     expect(productName).toEqual('Almond Toe Court Shoes, Patent Black');
   });
 
   it('should be able to display an "Add to Cart" button', function() {
     var addToCartButton = element(by.className('add-to-cart'));
-    expect(addToCartButton.getText()).toEqual('Add to Cart');
+    expect(addToCartButton.isPresent()).toEqual(true);
+  });
+
+  it('should be able to add an item to the cart', function() {
+    var addToCartButton = element(by.className('add-to-cart'));
+    var removeFromCartButton = element(by.className('remove-from-cart'));
+
+    addToCartButton.click();
+
+    expect(removeFromCartButton.isPresent()).toEqual(true);
+  });
+
+  it('should be able to remove an item from the cart', function() {
+    var addToCartButton = element(by.className('add-to-cart'));
+    var removeFromCartButton = element(by.className('remove-from-cart'));
+
+    addToCartButton.click();
+    removeFromCartButton.click();
+
+    expect(removeFromCartButton.isPresent()).toEqual(false);
   });
 });
